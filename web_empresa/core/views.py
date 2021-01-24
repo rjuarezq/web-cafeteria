bad2b5cc2db40d09a3c2079fd7b024361f399a9f
Ricardo Juarez <juarezqricardo@gmail.com>
2021-01-24 17:29:12 -0500

    templates linked to views

--- web_empresa/core/views.py
+++ web_empresa/core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'core/index.html')
+
+def about(request):
+    return render(request,'core/about.html')
+
+def services(request):
+    return render(request, 'core/services.html')
+
+def store(request):
+    return render(request, 'core/store.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def blog(request):
+    return render(request, 'core/blog.html')
