368bf74b1b99f3954a84bf502b1265de749045be
Ricardo Juarez <juarezqricardo@gmail.com>
2021-01-28 01:06:22 -0500

    rendering our data for services.html that have been obtained from models.py

--- web_empresa/services/views.py
+++ web_empresa/services/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Service
 # Create your views here.
 def services(request):
-    return render(request, 'services/services.html')
+    services = Service.objects.all()
+    return render(request, 'services/services.html', {'services' : services})
