a3726e949c3f74011915be3b99d7682588d5deda
Ricardo Juarez <juarezqricardo@gmail.com>
2021-02-01 13:16:15 -0500

    adding the category and blog view  also send the information that has been extracted from the database

--- web_empresa/blog/views.py
+++ web_empresa/blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
 
 # Create your views here.
+def posts(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/blog.html', {'posts': posts })
+
+def category(request, category_id):
+    #category = Category.objects.get(id=category_id)
+    
+    #Establecemos el id de la categoria
+    category = get_object_or_404(Category, id=category_id)
+    #Mostramos los posts que pertenecen a dicha categoria seleccionada
+    #posts = Post.objects.filter(categories = category)
+    
+    #return render(request, 'blog/category.html', {'category':category, 'posts': posts } )
+    return render(request, 'blog/category.html', {'category':category})
+                                                    
